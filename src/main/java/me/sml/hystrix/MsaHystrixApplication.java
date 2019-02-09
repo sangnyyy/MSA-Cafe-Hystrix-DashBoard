774846b03497531febeba3c11eacc67b9451aea7
SML <dltkdals2202@gmail.com>
2019-02-10 01:51:25 +0900

    feat: project added
    Signed-off-by: SML <dltkdals2202@gmail.com>

---
+++ src/main/java/me/sml/hystrix/MsaHystrixApplication.java
@@ -0,0 +1,14 @@
+package me.sml.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaHystrixApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/me/sml/hystrix/MsaHystrixApplicationTests.java
@@ -0,0 +1,17 @@
+package me.sml.hystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsaHystrixApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
